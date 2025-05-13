f0d005ecc2cb3843085bafed7ebf68bf13dbe6ef
Mr-Eke <c.eke@alustudent.com>
2025-05-13 10:29:58 +0200

    Test Basemodel: proper assignment and attribute data types

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,24 +1,44 @@
 import unittest
 from models.base_model import BaseModel
 import uuid
+from datetime import datetime
 
 class TestBaseModel(unittest.TestCase):
-    """ Test cases for the base model class and its attributes"""
+    """ Test case for the BaseModel class in the 'models' module."""
 
     def setUp(self):
-        self.base_model_obj = BaseModel()
-        self.id_str_obj = self.base_model_obj.id
+        """Set up the initial state for the tests."""
+        self.obj_1 = BaseModel()
+        self.obj_2 = BaseModel()
 
-    def test_obj_initialization(self):
-        self.assertTrue(isinstance(self.base_model_obj, BaseModel))
+    def tearDown(self):
+        """Clean up resources after each test."""
+        del self.obj_1
+        del self.obj_2
 
-    def test_attributes(self):
-        """ tests all attributes of basemodel"""
-        id_uuid_obj = uuid.UUID(self.id_str_obj)
+    def test_instantiation(self):
+        """Test if BaseModel objects are instantiated correctly."""
+        self.assertIsInstance(self.obj_1, BaseModel)
 
-        self.assertIsInstance(id_uuid_obj, uuid.UUID)
-        self.assertIsInstance(self.id_str_obj, str)
-        
+    def test_attr_type(self):
+        """Test the data types of specific attributes of BaseModel objects."""
+        with self.subTest():
+            self.assertIsInstance(self.obj_1.id, str)
+        with self.subTest():
+            self.assertIsInstance(self.obj_1.updated_at, datetime)
+
+    def test_unique_id(self):
+        """ Test if the id attribute of different
+        BaseModel instances is unique.
+        """
+        self.assertNotEqual(self.obj_1.id, self.obj_2.id)
+
+    def test_id_assignment(self):
+        """ validate that The id attribute of the BaseModel object is a
+        valid UUID.
+        """
+        self.assertTrue(uuid.UUID(self.obj_1.id, version=4))
+
 
 
 
