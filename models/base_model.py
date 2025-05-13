cf9687752e876e6b01993bce0e0f1deb65d19247
Mr-Eke <c.eke@alustudent.com>
2025-05-13 11:29:58 +0200

    to_dict: added a method for data serializing and desializing

--- models/base_model.py
+++ models/base_model.py
@@ -22,5 +22,20 @@ class BaseModel:
         """ returns a human readable str form of the class instance & attr"""
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
+    def save(self):
+        """ Updates the public instance attributes """
+        self.updated_at = datetime.now()
 
+    def to_dict(self):
+        """ returns a dictionary containing all keys/values of the instance
+        used for data serialization and deserialization
+        """
+        the_dict = {}
+        the_dict['__class__'] = self.__class__.__name__
 
+        for key, val in self.__dict__.items():
+            if key in ('created_at', 'updated_at'):
+                the_dict[key] = val.isoformat()  # convert to str obj
+            else:
+                the_dict[key] = val
+        return the_dict
