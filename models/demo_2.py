e79da4711dea095ada7d22fdedd0b6dd00ce2d18
Mr-Eke <c.eke@alustudent.com>
2025-06-20 17:54:12 +0200

    tests and console implementation

---
+++ models/demo_2.py
@@ -0,0 +1,24 @@
+
+
+# user_3 =  {
+#     "name": "ago",
+#     "age": 10,
+#     "net_worth": "$1b"
+# }
+
+#  user -> json(user) -> Db
+
+# python(user)
+
+#
+
+
+from datetime import datetime
+
+today_date = datetime.now()
+
+print(today_date)
+
+
+# boodk = datetime.mow()
+
